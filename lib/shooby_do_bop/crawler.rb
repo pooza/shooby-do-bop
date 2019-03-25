b01607ba784768380e50f37281fcfc8f0ebcd4b5
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-03-26 07:12:37 +0900

    #30

--- lib/shooby_do_bop/crawler.rb
+++ lib/shooby_do_bop/crawler.rb
@@ -15,6 +15,7 @@ module ShoobyDoBop
       @logger.info(params)
     rescue => e
       e = Ginseng::Error.create(e)
+      e.package = Package.full_name
       Slack.broadcast(e.to_h)
       @logger.error(e.to_h)
     end
