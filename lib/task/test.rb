4b7dc555cd2e531eaf0b5b599336ff43c541a445
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-08-19 09:47:43 +0900

    ginseng-core 1.4.5

--- lib/task/test.rb
+++ lib/task/test.rb
@@ -2,7 +2,7 @@ desc 'test all'
 task :test do
   ENV['TEST'] = ShoobyDoBop::Package.name
   require 'test/unit'
-  Dir.glob(File.join(ShoobyDoBop::Environment.dir, 'test/*')).each do |t|
+  Dir.glob(File.join(ShoobyDoBop::Environment.dir, 'test/*.rb')).each do |t|
     require t
   end
 end
